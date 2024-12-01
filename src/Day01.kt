27fa61b4fdc5d9b237a347a04eeaf22218f09f15
Aldo Malerba <aldomalerba.dev@gmail.com>
2024-12-01 19:15:13 +0100

    day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,37 @@
+import kotlin.math.abs
+
 fun main() {
+
+    fun day01Parser(input: List<String>) = input.map {
+        val parts = it.split(" ")
+        Pair(parts.first().toInt(), parts.last().toInt())
+    }.unzip()
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (leftSide, rightSide) = day01Parser(input)
+
+        val leftSideSorted = leftSide.sorted()
+        val rightSideSorted = rightSide.sorted()
+
+        val sizes = leftSideSorted.mapIndexed { index, left ->
+            val right = rightSideSorted[index]
+            abs(left - right)
+        }
+        return sizes.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (firstSide, secondSide) = day01Parser(input)
+
+        val scores = firstSide.map { left -> secondSide.count { it == left} * left }
+        return scores.sum()
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
-    // Read the input from the `src/Day01.txt` file.
+
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
