9e4054c8074366fb9c0ac0e9b07e1fc0baea1978
Aldo Malerba <aldomalerba.dev@gmail.com>
2024-12-06 22:23:49 +0100

    day06 improved performance

--- src/main/Day06.kt
+++ src/main/Day06.kt
@@ -2,8 +2,6 @@ class GuardStuckException : Throwable()
 
 class Day06: Day {
     override fun part1(input: List<String>): Int {
-        val mapHeight = input.size
-        val mapLength = input.first().length
 
         val obstacles = mutableListOf<Pair<Int, Int>>()
         var guardPosition = 0 to 0
@@ -16,14 +14,31 @@ class Day06: Day {
         }
 
         val guard = Guard('^', guardPosition.first, guardPosition.second, obstacles)
-        val visitedPositions = preditctPositions(mapLength, mapHeight, guard)
+        val changeOrientationPositions = preditctPositions(guard)
+        val result = joinPath(changeOrientationPositions, input)
 
-        return visitedPositions.size
+        return result.size
+    }
+
+    private fun joinPath(
+        changeOrientationPositions: List<Pair<Pair<Int, Int>, Char>>,
+        input: List<String>
+    ): MutableSet<Pair<Int, Int>> {
+        val result = mutableSetOf<Pair<Int, Int>>()
+        for ((index, pos) in changeOrientationPositions.withIndex()) {
+            val (x, y) = pos.first
+            val (nextX, nextY) = changeOrientationPositions.getOrNull(index + 1)?.first ?: (null to null)
+            when (pos.second) {
+                '^' -> (y downTo (nextY ?: 0)).forEach { result.add(x to it) }
+                '>' -> (x..(nextX ?: (input.first().length - 1))).forEach { result.add(it to y) }
+                'v' -> (y..(nextY ?: (input.size - 1))).forEach { result.add(x to it) }
+                else -> (x downTo (nextX ?: 0)).forEach { result.add(it to y) }
+            }
+        }
+        return result
     }
 
     override fun part2(input: List<String>): Int {
-        val mapHeight = input.size
-        val mapLength = input.first().length
 
         val obstacles = mutableListOf<Pair<Int, Int>>()
         var guardPosition = 0 to 0
@@ -35,17 +50,21 @@ class Day06: Day {
             }
         }
 
-        var stuckCount = 0
 
         val guard = Guard('^', guardPosition.first, guardPosition.second, obstacles)
-        val paths = preditctPositions(mapLength, mapHeight, guard)
+        val changeOrientationPositions = preditctPositions(guard)
+        val paths = joinPath(changeOrientationPositions, input)
 
+        var stuckCount = 0
         for (position in paths) {
             try {
+                if(position == 7 to 9){
+                    println("asdas")
+                }
                 guard.obstacles = obstacles
                 guard.obstacles = obstacles + position
                 guard.reset('^', guardPosition.first, guardPosition.second)
-                preditctPositions(mapLength, mapHeight, guard)
+                preditctPositions(guard)
             }catch (e: GuardStuckException) {
                 stuckCount++
             }
@@ -61,13 +80,12 @@ fun main() {
     val day06 = Day06()
 
     val input = readInput("Day06")
-    day06.part1(input).println()
-
     val startTime = System.currentTimeMillis()
+    day06.part1(input).println()
     day06.part2(input).println()
     val endTime = System.currentTimeMillis()
     val elapsedTimeInSeconds = (endTime - startTime) / 1000.0
-    println("Tempo di esecuzione: $elapsedTimeInSeconds secondi")
+    println("Time: $elapsedTimeInSeconds second")
 }
 
 class Guard(facing: Char, x: Int, y: Int, var obstacles: List<Pair<Int,Int>> = emptyList()) {
@@ -88,22 +106,38 @@ class Guard(facing: Char, x: Int, y: Int, var obstacles: List<Pair<Int,Int>> = e
         return x to y
     }
 
-    fun move() {
+    fun move(): Pair<Int, Int> {
         var nextX = x
         var nextY = y
 
         when(orientation){
-            '^' -> nextY--
-            'v' -> nextY++
-            '<' -> nextX--
-            else -> nextX++
+            '^' -> {
+                val nextPosition = obstacles.filter { it.first == nextX && nextY > it.second }.maxByOrNull { it.second }
+                nextY = if(nextPosition != null) nextPosition.second + 1 else -1
+                turn()
+            }
+            'v' -> {
+                val nextPosition = obstacles.filter { it.first == nextX && nextY < it.second }.minByOrNull { it.second }
+                nextY = if(nextPosition != null) nextPosition.second -1 else -1
+                turn()
+            }
+            '<' -> {
+                val nextPosition = obstacles.filter { it.second == nextY && nextX > it.first }.maxByOrNull { it.first }
+                nextX = if(nextPosition != null) nextPosition.first + 1 else -1
+                turn()
+            }
+            else -> {
+                val nextPosition = obstacles.filter { it.second == nextY && nextX < it.first }.minByOrNull { it.first }
+                nextX = if(nextPosition != null) nextPosition.first - 1 else -1
+                turn()
+            }
         }
 
-        if(obstacles.contains(nextX to nextY)) turn()
-        else {
-            x = nextX
-            y = nextY
-        }
+
+        x = nextX
+        y = nextY
+
+        return nextX to nextY
     }
 
     fun reset(resetFacing: Char, resetX: Int, resetY: Int,) {
@@ -115,29 +149,24 @@ class Guard(facing: Char, x: Int, y: Int, var obstacles: List<Pair<Int,Int>> = e
 }
 
 
-fun preditctPositions(mapLenght: Int,mapHeight: Int, guard: Guard): Set<Pair<Int, Int>> {
+fun preditctPositions(guard: Guard): List<Pair<Pair<Int, Int>,Char>> {
 
-        var setPositions = mutableSetOf(guard.position())
-        val allPositions = mutableListOf(guard.position())
+        val allPositions = mutableListOf(guard.position() to guard.orientation)
 
         var outside = false
         while (!outside){
-            val prevPosition = guard.position()
-            guard.move()
-            val (x, y) = guard.position()
-            if(prevPosition == x to y) allPositions.add(x to y)
-            if(x < 0 || y < 0 || x == mapLenght || y == mapHeight) outside = true
-            else setPositions.add(guard.position())
+            val (x, y) = guard.move()
+            if(x < 0 || y < 0) outside = true else allPositions.add((x to y) to guard.orientation)
 
             var checkSize = 8
             while(checkSize<=allPositions.size){
                     val lastEqualsElements = (allPositions.size - 1 downTo allPositions.size - checkSize/2).count { index ->
-                        allPositions[index] == allPositions[index - checkSize/2]
+                        allPositions[index].first == allPositions[index - checkSize/2].first
                     }
                     if(lastEqualsElements == checkSize/2) throw GuardStuckException()
                 checkSize+=2
             }
         }
 
-        return setPositions
+        return allPositions
 }
